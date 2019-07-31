d8b9f03aa85bb3fd736bad896171e5efa32ff948
Liam Setterlund <liamsett@gmail.com>
2019-07-31 17:22:23 -0400

    Added smooth double jump. Dash still needs work. Added another script.

---
+++ Assets/Scripts/SphereScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float speed = 10f;
+    public float jumpSpeed = 16f;
+    public float gravity = 40f;
+    public Vector3 moveDirection = Vector3.zero;
+    int jumpCount = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print ("Testing...");
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(controller.isGrounded);
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                jumpCount = jumpCount + 1;
+                print(jumpCount);
+            }
+        }
+        
+        if (!controller.isGrounded)
+        {
+
+            if ((Input.GetButtonDown("Jump")) && (jumpCount == 1))
+            {
+                moveDirection.y = jumpSpeed;
+                jumpCount = jumpCount + 1;
+                print(jumpCount);
+            }
+
+            if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
+            {
+                moveDirection.x = -speed;
+            }
+            if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+            {
+                moveDirection.x = speed;
+            }
+
+        }
+
+
+
+
+            //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 5, Input.GetAxis("Jump") * Time.deltaTime * 5, 0f);
+            //transform.Translate(Input.SetAxis())
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/charDash.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charDash : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    public float speed;
+    public float dashSpeed;
+    public float dashTime;
+    public float startDashTime;
+    int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject sphere = GameObject.Find("Sphere");
+        SphereScript sphereScript = sphere.GetComponent<SphereScript>();
+        rb = GetComponent<Rigidbody>();
+        moveDirection = sphereScript.moveDirection;
+        speed = sphereScript.speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+            {
+                direction = 2;
+            }
+        } else
+        {
+            if(dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector3.zero;
+            } else
+                {
+                dashTime -= Time.deltaTime;
+
+                if ((moveDirection.x == -speed) && Input.GetKeyDown("left shift"))
+                {
+                    rb.velocity = Vector3.left * dashSpeed;
+
+                } else if ((moveDirection.x == speed) && Input.GetKeyDown("left shift"))
+                {
+                     rb.velocity = Vector3.right * dashSpeed;
+                    }
+            }
+        }
+    }
+}
